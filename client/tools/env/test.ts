7a13583f846a846da277d53749fe6982fe56919d
quang.nguyen <quang@pretagov.com>
2018-03-23 15:04:06 +1100

    Change port

---
+++ client/tools/env/test.ts
@@ -0,0 +1,10 @@
+import { EnvConfig } from './env-config.interface';
+
+const TestConfig: EnvConfig = {
+  API_ENDPOINT: 'http://54.177.51.246:5001/api',
+  ENV: 'DEV',
+  VERSION: '1.0.0'
+};
+
+export = DevConfig;
+
