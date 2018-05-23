0a065acbc367d8ae0dd2bf88282cd54157a75066
quang.nguyen <quang@pretagov.com>
2018-05-24 00:48:55 +1000

    Add production config

--- client/tools/env/prod.ts
+++ client/tools/env/prod.ts
@@ -1,7 +1,7 @@
 import { EnvConfig } from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
-  API_ENDPOINT: 'http://192.168.0.250:5002',
+  API_ENDPOINT: 'http://192.168.0.250:5002/api',
   ENV: 'PROD',
   VERSION: '1.0.0'
 };
